d906f56ecca0f79c2ceb1b17bb1f688523a4c3d4
fuzh <fzh19860505>
2013-11-14 15:32:05 +0800

    a

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -21,24 +21,24 @@ worker_processes 4
 
 # Help ensure your application will always spawn in the symlinked
 # "current" directory that Capistrano sets up.
-working_directory "/data/www/wizshop" # available in 0.94.0+
+working_directory "/data/www/shopror" # available in 0.94.0+
 
 # listen on both a Unix domain socket and a TCP port,
 # we use a shorter backlog for quicker failover when busy
-listen "/data/www/wizshop/tmp/sockets/unicorn.sock", :backlog => 64
+listen "/data/www/shopror/tmp/sockets/unicorn.sock", :backlog => 64
 listen 8080, :tcp_nopush => true
 
 # nuke workers after 30 seconds instead of 60 seconds (the default)
 timeout 30
 
 # feel free to point this anywhere accessible on the filesystem
-pid "/data/www/wizshop/tmp/unicorn.pid"
+pid "/data/www/shopror/tmp/unicorn.pid"
 
 # By default, the Unicorn logger will write to stderr.
 # Additionally, ome applications/frameworks log to stderr or stdout,
 # so prevent them from going to /dev/null when daemonized here:
-stderr_path "/data/www/wizshop/log/unicorn.stderr.log"
-stdout_path "/data/www/wizshop/log/unicorn.stdout.log"
+stderr_path "/data/www/shopror/log/unicorn.stderr.log"
+stdout_path "/data/www/shopror/log/unicorn.stdout.log"
 
 # combine Ruby 2.0.0dev or REE with "preload_app true" for memory savings
 # http://rubyenterpriseedition.com/faq.html#adapt_apps_for_cow
