d9a9cc8293f3c930f7c35723f94a801aeb83180b
fuzh <fzh19860505>
2013-11-14 16:48:31 +0800

    product variant add RMB/kg

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,12 +5,13 @@ class CategoriesController < ApplicationController
   # GET /categories
   # GET /categories.json
   def index
-    @categories = Category.all
+    @categories = Category.all.page(params[:page] || 1).per(12)
   end
 
   # GET /categories/1
   # GET /categories/1.json
   def show
+    @category_products = @category.products.page(params[:page] || 1).per(10)
   end
 
   # GET /categories/new
