47880508040c2235a1e7bfc410987f4ecfc6866c
fuzh <fzh19860505>
2013-11-21 14:25:06 +0800

    product variant add RMB/kg

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,11 @@ Shopror::Application.routes.draw do
       get :featured
     end
   end
+  resources :suppliers do
+    collection do
+      get :origin
+    end
+  end
   resources :carts do
     resources :cart_items
     collection do
